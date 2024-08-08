ec90fa57c8ae569e5541429a4d515e754579cc6b
RRedBaron <nedelchevevg@gmail.com>
2024-08-08 20:35:13 +0300

    Start logic + questions displaying

---
+++ index.js
@@ -0,0 +1,78 @@
+let intro_container = document.querySelector(".intro-container");
+let question_container = document.querySelector(".question-container");
+let start_button = document.querySelector(".start-button");
+
+start_button.addEventListener("click", function () {
+  intro_container.style.display = "none";
+  question_container.style.display = "flex";
+  start_quiz();
+});
+
+let questions;
+fetch("./questions.json")
+  .then((response) => response.json())
+  .then((data) => {
+    questions = data.questions;
+  });
+
+let correct_answers = 0;
+let current_question = 0;
+let current_question_correct; // змінна, яка зберігає поточну правильну відповідь
+let hp = 3;
+
+let heart_counter = document.querySelector(".heart-counter");
+let question_text = document.querySelector(".question-text");
+let time_counter = document.querySelector(".timer-counter");
+let cards = document.querySelectorAll(".card");
+let cards_counter = document.querySelector(".cards-counter");
+let correct_counter = document.querySelector(".correct-counter");
+
+function start_quiz() {
+  question_text.innerHTML = questions[current_question].word;
+  cards_counter.innerHTML = questions.length;
+  correct_counter.innerHTML = 0;
+  for (let i = 0; i < 3; i++) {
+    if (questions[current_question].options[i].correct) {
+      current_question_correct = questions[current_question].options[i].text;
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    cards[i].querySelector(".card-text").innerHTML =
+      questions[current_question].options[i].text;
+    cards[i].querySelector(".card-image").src =
+      questions[current_question].options[i].image_url;
+  }
+  setInterval(function () {
+    time_counter.innerHTML = +time_counter.innerHTML + 1;
+  }, 1000);
+}
+
+for (let i = 0; i < 3; i++) {
+  cards[i].addEventListener("click", function () {
+    cards_counter.innerHTML = +cards_counter.innerHTML - 1;
+    if (
+      cards[i].querySelector(".card-text").innerHTML == current_question_correct
+    ) {
+      console.log("Правильно!");
+      correct_answers++;
+      correct_counter.innerHTML = correct_answers;
+    } else {
+      console.log("Неправильно!");
+      hp--;
+      heart_counter.innerHTML = hp;
+    }
+    current_question++;
+    for (let i = 0; i < 3; i++) {
+      if (questions[current_question].options[i].correct) {
+        current_question_correct = questions[current_question].options[i].text;
+      }
+    }
+    question_text.innerHTML = questions[current_question].word;
+    for (let i = 0; i < 3; i++) {
+      cards[i].querySelector(".card-text").innerHTML =
+        questions[current_question].options[i].text;
+      cards[i].querySelector(".card-image").src =
+        questions[current_question].options[i].image_url;
+    }
+  });
+}
